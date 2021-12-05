85d19cc7b2af9e5c7b6e34020e0d6e3a2fc4627e
yoshida261 <yoshida261@gmail.com>
2021-12-05 15:15:58 +0900

    Add search input

--- JetRakuma/app/src/main/java/com/stah/jetrakuma/home/HomeComponents.kt
+++ JetRakuma/app/src/main/java/com/stah/jetrakuma/home/HomeComponents.kt
@@ -7,13 +7,18 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -38,6 +43,30 @@ fun JetRakumaBottomNavigation () {
             )
         }
     }
+}
+
+@ExperimentalComposeUiApi
+@Composable
+fun SearchInputText(
+    text: String,
+    onTextChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    onImeAction: () -> Unit = {}
+){
+    val keyboardController = LocalSoftwareKeyboardController.current
+    TextField(
+        value = text,
+        label = { Text("キーワードで検索")},
+        onValueChange = onTextChange,
+        colors = TextFieldDefaults.textFieldColors(backgroundColor = Color.Transparent),
+        maxLines = 1,
+        keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
+        keyboardActions = KeyboardActions(onDone = {
+            onImeAction()
+            keyboardController?.hide()
+        }),
+        modifier = modifier
+    )
 }
 
 @OptIn(ExperimentalFoundationApi::class)
--- JetRakuma/app/src/main/java/com/stah/jetrakuma/home/HomeScreen.kt
+++ JetRakuma/app/src/main/java/com/stah/jetrakuma/home/HomeScreen.kt
@@ -1,22 +1,48 @@
 package com.stah.jetrakuma.home
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.BottomNavigation
-import androidx.compose.material.BottomNavigationItem
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Icon
-import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
+@ExperimentalComposeUiApi
 @Composable
 fun HomeScreen() {
-    Column() {
-        // topbar todo
-
+    Column {
+        HomeSearchInput()
         PhotoGrid(modifier = Modifier.weight(1f))
         JetRakumaBottomNavigation()
     }
 }
 
+@ExperimentalComposeUiApi
+@Composable
+fun HomeSearchInput() {
+    Row() {
+        Box(
+            Modifier
+                .align(Alignment.CenterVertically)
+                .padding(start = 16.dp)
+        ) {
+            Icon(
+                Item.Search.icon, contentDescription = null,
+            //    modifier = Modifier.background(Color.Gray)
+            )
+        }
+        SearchInputText(
+            text = "",
+            onTextChange = {},
+            modifier = Modifier
+//                .padding(horizontal = 16.dp)
+               // .background(Color.Gray)
+                .weight(1f),
+            onImeAction = {}
+        )
+
+    }
+}
