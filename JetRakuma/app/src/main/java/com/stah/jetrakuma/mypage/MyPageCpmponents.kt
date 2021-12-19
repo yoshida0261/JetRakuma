88b97b63523f23916e3cfdc32fa9b6ad076567fe
yoshida261 <yoshida261@gmail.com>
2021-12-20 01:17:48 +0900

    Refactor mypage layout setting

--- JetRakuma/app/src/main/java/com/stah/jetrakuma/mypage/MyPageCpmponents.kt
+++ JetRakuma/app/src/main/java/com/stah/jetrakuma/mypage/MyPageCpmponents.kt
@@ -1,15 +1,21 @@
 package com.stah.jetrakuma.mypage
 
-import androidx.compose.material.Icon
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Money
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
@@ -19,40 +25,97 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.stah.jetrakuma.R
 import com.stah.jetrakuma.ui.theme.Ocean3
-import com.stah.jetrakuma.ui.theme.Ocean8
 import com.stah.jetrakuma.ui.theme.Shadow4
 
 @Composable
-fun Avatar(
-
+fun TopBar(
 ) {
-    Image(
-        painter = painterResource(R.drawable.bird),
-        contentDescription = "avatar",
-        contentScale = ContentScale.Crop,            // crop the image if it's not a square
+    Row(
+        horizontalArrangement = Arrangement.Center,
         modifier = Modifier
-            .size(64.dp)
-            .clip(CircleShape)                       // clip to the circle shape
-            .border(2.dp, Color.Gray, CircleShape)   // add a border (optional)
-    )
+            .fillMaxWidth()
+            .padding(top = 16.dp, start = 8.dp)
+    ) {
+        Column(
+
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Image(
+
+                painter = painterResource(R.drawable.bird),
+                contentDescription = "avatar",
+                contentScale = ContentScale.Crop,            // crop the image if it's not a square
+                modifier = Modifier
+                    .size(64.dp)
+                    .clip(CircleShape)                       // clip to the circle shape
+                    .border(2.dp, Color.Gray, CircleShape)   // add a border (optional)
+            )
+
+            Text(text = "プロフィール", Modifier.padding(top = 8.dp))
+        }
+        /*
+        Spacer(modifier = Modifier.size(16.dp))
+        Column(modifier = Modifier.weight(1F)) {
+
+            Icon(Icons.Default.WbSunny, contentDescription = "Localized description", tint = Color.Red)
+        }
+        Icon(Icons.Default.Notifications, contentDescription = "Localized description", tint = Color.Red)
+
+         */
+    }
 }
+
+@ExperimentalFoundationApi
+@Composable
+fun MyPageItems(modifier: Modifier) {
+    val list = (1..10).map { it.toString() }
+
+    LazyVerticalGrid(
+        cells = GridCells.Fixed(3),
+        modifier = modifier,
+
+        // content padding
+        contentPadding = PaddingValues(
+            start = 12.dp,
+            top = 16.dp,
+            end = 12.dp,
+            bottom = 16.dp
+        ),
+        content = {
+            items(list.size) { index ->
+                Card(
+                    backgroundColor = Color.White,
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .fillMaxHeight()
+                        .fillMaxWidth(),
+                    elevation = 8.dp,
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_launcher_background),
+                        contentDescription = "Test"
+                    )
+                    Text("Test")
+                }
+            }
+        }
+    )
+
+}
+
 
 @Composable
 fun Header() {
     Spacer(
         modifier = Modifier
-            .height(180.dp)
+            .height(108.dp)
             .fillMaxWidth()
             .background(Brush.horizontalGradient(listOf(Shadow4, Ocean3)))
     )
 }
 
 @Composable
-fun MypageMenu(){
-
-
-    Icon(Icons.Filled.Money, null, )
-    Icon(Icons.Filled.ShoppingCart, null, )
-
-
+fun MypageMenu() {
+    Icon(Icons.Filled.Money, null)
+    Icon(Icons.Filled.ShoppingCart, null)
 }
--- JetRakuma/app/src/main/java/com/stah/jetrakuma/mypage/MyPageScreen.kt
+++ JetRakuma/app/src/main/java/com/stah/jetrakuma/mypage/MyPageScreen.kt
@@ -1,18 +1,21 @@
 package com.stah.jetrakuma.mypage
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
+@ExperimentalFoundationApi
 @Composable
 fun MyPageScreen() {
-    Box(Modifier.fillMaxSize()) {
-        Header()
-        MypageMenu()
-        Column() {
-            Spacer(modifier = Modifier.height(24.dp))
-            Avatar()
-        }
+    //Header()
+    Column(Modifier.fillMaxSize()) {
+//            Spacer(modifier = Modifier.height(24.dp))
+        TopBar()
+        MyPageItems(modifier = Modifier.padding(start = 16.dp, end = 16.dp))
+
     }
 }
